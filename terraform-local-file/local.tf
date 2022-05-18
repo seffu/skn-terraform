a1de0bdcbf9920fa7696119cb31b7c6153332451
seffu <seffukioi@gmail.com>
2022-05-18 14:24:34 +0300

    added terraform-local-file

---
+++ terraform-local-file/local.tf
@@ -0,0 +1,13 @@
+resource "local_file" "pet"{
+    filename = var.filename
+    content = "my favourite pet is ${random_pet.my-pet.id}"
+    depends_on = [
+      random_pet.my-pet
+    ]
+}
+
+resource "random_pet" "my-pet" {
+  prefix = var.prefix
+  separator = var.separator
+  length = var.length
+}
---
+++ terraform-local-file/variabes.tf
@@ -0,0 +1,17 @@
+variable "filename" {
+  default = "/home/skn/Desktop/Projects/DEVOPS/TERRAFORM/terraform-local-file/pet.txt"
+}
+variable "content" {
+  default = "Check for changes"
+}
+variable "prefix" {
+  default = "Ms"
+}
+  
+variable "separator" {
+  default = "."
+}
+
+variable "length" {
+  default = "1"
+}
